5db78cd1c3c78f76a569d942761540bdadcf4c14
Zak <zakariabouaddou@gmail.com>
2024-09-16 18:27:41 -0700

    Add project files.

---
+++ Activity12.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int positive_num;
+
+    cout << "Enter a postive number: " << endl;
+    cin >> positive_num;
+
+    for (int i = positive_num - 1; i > 1; i--)
+        positive_num *= i;
+        cout << positive_num;
+
+    cout << positive_num;
+
+}
