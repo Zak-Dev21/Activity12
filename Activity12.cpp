fd69ec6e0b2d8d9ff3d9d60f3a73c6d6da3ac06c
Zak <zakariabouaddou@gmail.com>
2024-09-16 19:18:30 -0700

    modified program to display numbers used to calculate factorial.

--- Activity12.cpp
+++ Activity12.cpp
@@ -8,14 +8,14 @@ int main()
 {
     int positive_num;
 
-    cout << "Enter a postive number: " << endl;
+    cout << "Enter a positive number: " << endl;
     cin >> positive_num;
-
-    for (int i = positive_num - 1; i > 1; i--) {
+    cout << positive_num;
+    for (int i = positive_num - 1; i >= 1; i--) {
+        cout << " * " << i;
         positive_num *= i;
-        cout << i << endl;
     }
 
-    cout << positive_num;
+    cout << " = " << positive_num;
 
 }
